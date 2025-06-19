8bc740f858e4a6a0f14df03e1297aa765dccd5a5
Clark Winters <github@clarkwinters.com>
2025-06-19 12:36:04 -0500

    functional bastion + VM connectivity

--- terraform/bastion.tf
+++ terraform/bastion.tf
@@ -5,14 +5,23 @@ resource "azurerm_subnet" "bastion" {
   address_prefixes     = ["10.0.2.0/24"]
 }
 
-# This doesn't actually work to connect to the VM.
-# Error when trying to connect: "There was an error in requesting a session. Please try again."
-# Error in the dev console: "Failed to get a data pod from the brain for bastion - omnibrain.centralus.bastionglobal.azure.com"
-# Not sure if this is a bug in Azure or if something is misconfigured.
+resource "azurerm_public_ip" "bastion" {
+  name                = "${local.name}-bastion-ip"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  allocation_method   = "Static"
+}
+
+# A Developer SKU doesn't work to connect to the VM.
 resource "azurerm_bastion_host" "bastion" {
   name                = "${local.name}-bastion"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
-  sku                 = "Developer"
-  virtual_network_id  = azurerm_virtual_network.main.id
+  sku                 = "Basic"
+
+  ip_configuration {
+    name                 = "${local.name}-bastion-ip"
+    subnet_id            = azurerm_subnet.bastion.id
+    public_ip_address_id = azurerm_public_ip.bastion.id
+  }
 }
